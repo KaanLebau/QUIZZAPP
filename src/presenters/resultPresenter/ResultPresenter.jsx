da1be7e2c3b5b784ec1e0012479d746306872df1
Lillfrallan <76948186+Lillfrallan@users.noreply.github.com>
2023-05-10 17:03:04 +0200

    This can maybe update firestore.

--- src/presenters/resultPresenter/ResultPresenter.jsx
+++ src/presenters/resultPresenter/ResultPresenter.jsx
@@ -3,15 +3,17 @@ import { useNavigate } from "react-router-dom";
 import ResultView from "../../views/resultView/ResultView";
 import { useLocation } from 'react-router-dom';
 import QuestionsView from "../../views/questionsView/QuestionsView";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { activeQuizState, activeUser } from "../../models/atoms";
 import { updateUser } from "../../models/utilities";
+import { RemoteStorage } from "../../integration/RemoteStorage";
 import "./resultPresenter.scss"
 
 function ResultPresenter() {
   const [takenQuiz, setTakenQuiz] = useRecoilState(activeQuizState);
   const [userData, setUserData] = useRecoilState(activeUser);
   const result = useLocation().state;
+  const db = RemoteStorage();
   const answerDistribution = {
     correct: result.correct,
     wrong: result.wrong,
@@ -21,9 +23,11 @@ function ResultPresenter() {
   const navigate = useNavigate();
 
   function handleResult() {
-    let updatedUser = updateUser(userData, result)
-    setUserData(updatedUser);
-    console.log(userData);
+    let updatedUser = updateUser(userData, result);
+    db.updateRemoteStorageFromModel({
+      field: result.category.toLowerCase(), 
+      data: updatedUser[result.category.toLowerCase()] 
+    });
     setTakenQuiz({})
     navigate("../../");
   }
